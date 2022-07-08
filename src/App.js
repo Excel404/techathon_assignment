d51829d7aa451fa68c238851873062a429ea8360
Excel <edehexcel2000@gmail.com>
2022-07-08 23:07:03 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Form } from 'react-bootstrap';
 import './App.css';
 import Homepage from './components/Homepage/Homepage';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    
-     <Homepage/> 
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+
     
   );
 }
